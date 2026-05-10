19ecaa8a6bdc2b46637fd764d2b9a97a148f34c8
cobotech <cobotech2@gmail.com>
2026-05-11 01:39:53 +0900

    3rd commit :add functions and add prototype at header file

---
+++ ft_memcpy.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+void *ft_memcpy(void *dest, const void *src, size_t n){
+  unsigned char * d;
+  unsigned char * s;
+  size_t i ;
+
+  d = (unsigned char *) dest;
+  s = (unsigned char *)src;
+  i = 0;
+
+  if (d==NULL&&s==NULL){
+    return(NULL);
+  }
+  while(i<n){
+    d[i]=s[i];
+    i++;
+  }
+  return(dest);
+}
